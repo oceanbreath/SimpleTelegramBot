525a8f4fbd886bc104c792ba12252fab25be9a59
oceanbreath <olefero@gmail.com>
2024-06-19 21:54:53 +0200

    first commit

---
+++ src/main/java/net/oceanbreath/SimpleTelegramBot/config/BotConfig.java
@@ -0,0 +1,19 @@
+package net.oceanbreath.SimpleTelegramBot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource(value = "classpath:application.properties")
+public class BotConfig {
+
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.token}")
+    String token;
+
+}
---
+++ src/test/java/net/oceanbreath/SimpleTelegramBot/SimleTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package net.oceanbreath.SimpleTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimleTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
